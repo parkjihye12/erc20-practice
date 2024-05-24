9d15540aa4f115a4e835a977e51893a577c6a164
parkjihye12 <qkr-wlg@hanmail.net>
2024-05-24 10:26:15 +0900

    Metamask Log In

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,5 +1,51 @@
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  const [signer, setSigner] = useState();
+
+  const onClickMetamask = async () => {
+    try {
+      if (!window.ethereum) return;
+
+      const provider = new ethers.BrowserProvider(window.ethereum);
+
+      setSigner(await provider.getSigner());
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onClickLogOut = () => {
+    setSigner(null);
+  };
+
+  useEffect(() => {
+    console.log(signer);
+  }, [signer]);
+
+  return (
+    <div className="min-h-screen flex flex-col justify-start items-center py-16">
+      {signer ? (
+        <div className="flex gap-8">
+          <div className="box-style">
+            안녕하세요, {signer.address.substring(0, 7)}...
+            {signer.address.substring(signer.address.length - 5)}님
+          </div>
+          <button
+            className="button-style border-red-300 hover:border-red-400"
+            onClick={onClickLogOut}
+          >
+            로그아웃
+          </button>
+        </div>
+      ) : (
+        <button className="button-style" onClick={onClickMetamask}>
+          🦊 메타마스크 로그인
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default App;
